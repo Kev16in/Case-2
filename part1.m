6449fab2f1b8615b8513b8f734b1e421a749d1c7
Kev16in <gutierrezvelasco@wustl.edu>
2022-11-01 19:17:45 -0500

    w

--- part1.m
+++ part1.m
@@ -1,12 +1,24 @@
-SIRD = [0.9;0.08;0.00;0.02];
+%%% The functions used for this part can all be found in this script
+%%
+
+%%% This is one of our first attempts at making a linear dynamical model
+%%% where we plugged in a starting percentage for each letter (Susceptible
+%%% , Infected, recover, Deceased) and a rate at which they change as time
+%%% passes on. 
+
+SIRD = [0.9;0.08;0.00;0.02]; 
+
+% Here we have the starting percentagaes 
 
 stateChanger = [0.85 0.1 0 0; 0.15 0.75 0 0;...
     0 0.1 1 0; 0 0.05 0 1];
 
+% Above is the A matrix that changes x as it takes t steps
+
 changeSIRD = [];
 change=stateChanger*SIRD; %%Calculate change for first week
 changeSIRD=cat(2,changeSIRD,change); %%Concatenate change vector to empty array
-for i = 1:9
+for i = 1:9 
     change=stateChanger*change; %%use the current change vector to get new change vector
     changeSIRD=cat(2,changeSIRD,change); %%Concatenate change vector to empty array
 end 
@@ -15,7 +27,13 @@ title('Simulation of Dynamical Model (No reinfections)')
 legend('Susceptible','Infected','Recovered','Deceased');
 xlabel("Time in Months");
 ylabel("Fraction of Population");
+
+% This is the graph of what our simulation gave us based on the parameters
+% and initial conditions. 
 %%
+%%% Here we have a different attempt where we introduce the ability to
+%%% become reinfected
+
 changeMatrix=[0.85 0.1 0 0;
     0.15 0.75 0.05 0;
     0 0.1 0.95 0;
@@ -24,10 +42,12 @@ newSIRD=[1;0;0;0];
 change2=changeMatrix*newSIRD;
 changeSIRD2=[];
 changeSIRD2=cat(2,changeSIRD2,change2);
+
 for i = 1:99
     change2=changeMatrix*change2; %%use the current change vector to get new change vector
     changeSIRD2=cat(2,changeSIRD2,change2); %%Concatenate change vector to empty array
 end
+
 figure;
 plot(changeSIRD2');
 title('Simulation of dynamical model (Reinfections Allowed)')
